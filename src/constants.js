65b280a33edb9410681fe663a9a8003062bff2e5
Liopold35894 <lic028@ucsd.edu>
2025-01-09 18:01:34 -0800

    Initial commit of 2D portfolio project

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+    pc: `This is my PC. I use it to code, learn, play games, and watch videos.
+        I work mostly with Java. I am also learning JavaScript, React, and Node.js, and other front end technologies.
+        I've also made a few games with Java, Unity, and Kaboom.js. You can find my projects on my <a href="https://github.com/Liopold35894" target="_blank">GitHub</a>!`,
+    "cs-degree": `My name is Liopold Chen, and I graduated from UC San Diego with a Bachelor's degree in Computer Science.
+         This is my degree! I hunged it up on the wall to remind myself what I've accomplished, and to motivate myself to keep learning and growing.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+        I've also made some AI music video on YouTube with SunoAI, and attended some AI music 2pick competitions held by streamer <a href="https://www.youtube.com/@tolocat" target="_blank">老皮 (Opchannel)</a>.
+        You can find the videos here on my <a href="https://www.youtube.com/@liopold35894" target="_blank">YouTube channel</a>!`,
+    tv: `That's my TV. I rarely use it nowaday since I watch most of my videos on my PC, but sometime using big screen to play games is nice!`,
+    bed: `This is where I sleep. I really like to read either it's novel or manga before I go to bed. It releases my stress and helps me to sleep better.`,
+    resume: `This is my desk and on it is my resume. I am interested in software engineering, web development, and game development.
+         Make sure to check it out <a href="https://github.com/Liopold35894/Resume" target="_blank">here</a>.
+         Contact me at liopoldchen@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+         Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. 
+         You can find my other projects on my <a href="https://github.com/Liopold35894" target="_blank">GitHub</a>!`,
+    library: `There is many genre of books on the bookshelf. I like to read novel, manga, and programming books.
+         One of my favorite novel is "Harry Potter" series, and one of my favorite manga is "Hunter x Hunter".`,
+    exit: `If you want to exit Liopold's portfolio, just close the tab.`
+}
